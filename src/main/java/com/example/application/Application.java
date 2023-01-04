447c460d597690646cfc9b0ee56bfc5f3def5ec2
Marcus Hellberg <marcus@vaadin.com>
2023-01-04 15:00:34 -0800

    add form and grid

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -1,9 +1,11 @@
 package com.example.application;
 
+import com.example.application.views.about.Person;
 import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -22,6 +24,7 @@ import org.springframework.context.annotation.ImportRuntimeHints;
 @PWA(name = "flow-native", shortName = "flow-native", offlineResources = {})
 @NpmPackage(value = "line-awesome", version = "1.3.0")
 @NpmPackage(value = "@vaadin-component-factory/vcf-nav", version = "1.0.6")
+@RegisterReflectionForBinding(Person.class)
 public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/application/views/about/AboutView.java
+++ src/main/java/com/example/application/views/about/AboutView.java
@@ -1,31 +1,42 @@
 package com.example.application.views.about;
 
 import com.example.application.views.MainLayout;
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.BeanValidationBinder;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+
+import java.util.List;
 
 @PageTitle("About")
 @Route(value = "about", layout = MainLayout.class)
 public class AboutView extends VerticalLayout {
 
+	TextField name = new TextField("Name");
+
+	EmailField email = new EmailField("Email");
+
 	public AboutView() {
-		setSpacing(false);
+		var binder = new BeanValidationBinder<>(Person.class);
+		binder.bindInstanceFields(this);
+		binder.readBean(new Person());
 
-		Image img = new Image("images/empty-plant.png", "placeholder plant");
-		img.setWidth("200px");
-		add(img);
+		var saveButton = new Button("Save", e -> {
+			var updated = new Person();
+			if (binder.writeBeanIfValid(updated)) {
+				Notification.show("Saved " + updated.getName());
+			}
+		});
 
-		add(new H2("This place intentionally left empty"));
-		add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+		var grid = new Grid<>(Person.class);
+		grid.setItems(List.of(new Person("Foo", "foo@bar.com"), new Person("Bar", "bar@bar.com")));
 
-		setSizeFull();
-		setJustifyContentMode(JustifyContentMode.CENTER);
-		setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-		getStyle().set("text-align", "center");
+		add(name, email, saveButton, grid);
 	}
 
 }
