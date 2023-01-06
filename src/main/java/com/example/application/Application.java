116f64e796fad89a41c745a2223b60bb4274a52e
Marcus Hellberg <marcus@vaadin.com>
2023-01-06 10:52:11 -0800

    simplified application class

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -2,24 +2,15 @@ package com.example.application;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
-import com.vaadin.flow.theme.Theme;
-import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * The entry point of the Spring Boot application.
- * <p>
- * Use the @PWA annotation to make the application installable on phones, tablets and some
- * desktop browsers.
- */
 @Push
 @SpringBootApplication
-@Theme(variant = Lumo.DARK)
 public class Application implements AppShellConfigurator {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 
 }
